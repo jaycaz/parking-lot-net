da581a25f458329789c64940d526258d798169d2
Jordan <jacazamias@gmail.com>
2016-03-12 15:49:37 -0800

    Fix batch loading in counter_stats

---
+++ get_counter_stats.lua
@@ -0,0 +1,73 @@
+-- Helper script to easily load statistics from pretrained lot counter model
+
+require 'torch'
+require 'nn'
+require 'DataLoader'
+stats = require("stats")
+
+params = {
+  h5_file = '/home/jordan/Documents/parking-lot-net/h5/lots.hdf5',
+  model_path = '/home/jordan/Documents/parking-lot-net/pretrained/counter_final',
+  train_set = 'nil',
+  labels = 'meta_count_spots',
+  max_spots = 20
+}
+
+local loader = DataLoader{h5_file = params.h5_file, train_cond=params.train_set, labels=params.labels, max_spots=params.max_spots}
+
+
+NUM_TRAIN = loader:getTrainSize()
+NUM_TEST = loader:getTestSize()
+NUM_VAL = loader:getValSize()
+
+net = torch.load(params.model_path)
+
+print("Pretrained model loaded")
+
+-- Print final train and validation statistics
+--print(string.format('Running model on train set (%d images)...', NUM_TRAIN))
+--local train, train_y = loader:getBatch{batch_size = NUM_TRAIN, split = 'train'}
+--print("Train set loaded")
+--local val, val_y = loader:getBatch{batch_size = NUM_VAL, split = 'val'}
+--print("Val set loaded")
+--local test, test_y = loader:getBatch{batch_size = NUM_TEST, split = 'test'}
+--print("Test set loaded")
+local train_idx, train_gt, num_labels = stats.classify(net:double(), loader, 'train')
+local val_idx, val_gt, _ = stats.classify(net:double(), loader, 'val')
+local test_idx, test_gt, _ = stats.classify(net:double(), loader, 'test')
+
+local train_acc = stats.acc(train_idx, train_gt, num_labels)
+local val_acc = stats.acc(val_idx, val_gt, num_labels)
+local test_acc = stats.acc(test_idx, test_gt, num_labels)
+
+-- Print test statistics
+print("**Train Acc,Val Acc,Test Acc")
+print(string.format("*%04f,%04f,%04f", train_acc, val_acc, test_acc))
+
+
+  -- Print all the test files that were incorrectly labeled
+--misclass_paths = stats.misclassified(net:double(), test:double(), paths, test_y:int())
+--for i, path in ipairs(misclass_paths) do
+  --print(path)
+--end
+
+-- Print confusion matrix parameters for test set
+local conf = stats.confusion_counter(test_idx, test_gt, num_labels)
+
+print('Confusion Matrix Statistics:')
+for k1,v1 in pairs(conf) do
+  for k2,v2 in pairs(v1) do
+    print(string.format('%s,%s,%f', k1, k2, v2))
+  end
+end
+
+-- Print error histogram
+  --local val, val_y = loader:getBatch{batch_size = NUM_VAL, split = 'val'}
+local errs = stats.error_matrix(test_idx, test_gt, num_labels)
+
+print('Error Histogram Statistics:')
+for k,v in pairs(errs) do
+  print(string.format('%s,%f', k, v))
+end
+
+
--- stats.lua
+++ stats.lua
@@ -1,44 +1,42 @@
 require 'torch'
 require 'math'
+require 'DataLoader'
 
-local stats = {}
+stats = {}
 
 -- Runs forward pass of <model> on <data> and returns accuracy
 -- based on groud truth labels <labels>
-function stats.acc(model, data, labels)
+function stats.acc(guesses, labels, num_labels)
   -- Find number of generated labels that match ground truth labels
-  local indices = classify(model, data)
+  data_size = labels:size(1)
+  --num_labels = labels:size(2)
 
-  local correct = torch.sum(torch.eq(indices, labels:long()))
-  local num_labels = labels:size(1)
+  correct = torch.sum(torch.eq(guesses, labels:long()))
+  --num_labels = labels:size(1)
 
   --for i = 1, labels:size(1) do
-    --print(string.format("Guess %d, Correct %d", indices[i], labels[i]))
+    --print(string.format("Guess %d, Correct %d", guesses[i], labels[i]))
   --end
 
-  return correct / num_labels
+  return correct / data_size
 
 end
 
 -- Returns necessary values for a confusion matrix
 -- Occupied counts as positive
-function stats.confusion(model, data, labels)
+function stats.confusion(guesses, labels, num_labels)
+  data_size = labels:size(1)
+  --num_labels = labels:size(2)
 
-  local score0 = model:forward(data:narrow(1,1,1))
-  local data_size = data:size(1)
-  local num_labels = score0:size(1)
-
-  local indices = classify(model, data)
-
-  local tp = torch.sum(torch.cmul(torch.eq(indices,2), torch.eq(labels:long(),2)))
-  local fp = torch.sum(torch.cmul(torch.eq(indices,2), torch.eq(labels:long(),1)))
-  local tn = torch.sum(torch.cmul(torch.eq(indices,1), torch.eq(labels:long(),1)))
-  local fn = torch.sum(torch.cmul(torch.eq(indices,1), torch.eq(labels:long(),2)))
+  tp = torch.sum(torch.cmul(torch.eq(guesses,2), torch.eq(labels:long(),2)))
+  fp = torch.sum(torch.cmul(torch.eq(guesses,2), torch.eq(labels:long(),1)))
+  tn = torch.sum(torch.cmul(torch.eq(guesses,1), torch.eq(labels:long(),1)))
+  fn = torch.sum(torch.cmul(torch.eq(guesses,1), torch.eq(labels:long(),2)))
 
   pn_total = tp + fp + tn + fn
   assert(pn_total == data_size, string.format('Confusion matrix parameters (%d) do not add up to total data size (%d)', pn_total, data_size))
 
-  local res = {true_pos = tp, false_pos = fp, true_neg = tn, false_neg = fn}
+  res = {true_pos = tp, false_pos = fp, true_neg = tn, false_neg = fn}
   for k,v in pairs(res) do
     res[k] = res[k] / data_size
   end
@@ -48,12 +46,11 @@ end
 
 
 -- Returns file paths of all misclassified images
-function stats.misclassified(model, data, paths, labels)
-  local misclass_paths = {}
-  local indices = classify(model, data)
+function stats.misclassified(guesses, paths, labels, num_labels)
+  misclass_paths = {}
 
-  for i = 1, indices:size(1) do
-    if indices[i] ~= labels[i] then
+  for i = 1, guesses:size(1) do
+    if guesses[i] ~= labels[i] then
       table.insert(misclass_paths, paths[i])
     end
   end
@@ -62,40 +59,13 @@ function stats.misclassified(model, data, paths, labels)
 end
 
 
-function classify(model, data)
-  local BATCH_SIZE = 100
-  local score0 = model:forward(data:narrow(1,1,1))
-  local data_size = data:size(1)
-  local num_labels = score0:size(1)
-  local scores = torch.Tensor(data_size, num_labels)
+-- For counter CNN, get histogram of ground truth vs. choices
+-- row is what the gt was, col is what the model guessed
+function stats.confusion_counter(guesses, labels, num_labels)
+  data_size = labels:size(1)
+  --num_labels = labels:size(2)
 
-  -- Process data in batches
-  local i = 1
-  while i < data_size do
-    b = math.min(BATCH_SIZE, data_size - i)
-    --print(data[{{i,i+b}}]:size())
-    scores[{{i,i+b}}] = model:forward(data[{{i,i+b}}])
-    i = i+b
-  end
-
-  -- Find number of generated labels that match ground truth labels
-  maxs, indices = torch.max(scores, 2)
-  indices = torch.reshape(indices, indices:size(1))
-
-  return indices
-end
-
-
--- For counter CNN, get histogram of choices vs ground truth
-function stats.confusion_counter(model, data, labels)
-
-  local score0 = model:forward(data:narrow(1,1,1))
-  local data_size = data:size(1)
-  local num_labels = score0:size(1)
-
-  local indices = classify(model, data)
-
-  local confusion_counts = {}
+  confusion_counts = {}
   for i = 1, num_labels do
     confusion_counts[i] = {}
     for j = 1, num_labels do
@@ -105,7 +75,7 @@ function stats.confusion_counter(model, data, labels)
 
   for i = 1, data_size do
     local gt = labels[i]
-    local guess = indices[i]
+    local guess = guesses[i]
     confusion_counts[gt][guess] = confusion_counts[gt][guess] + 1
   end
 
@@ -114,17 +84,15 @@ function stats.confusion_counter(model, data, labels)
 end
 
 --For counter CNN, get histogram of errors
-function stats.error_matrix(model, data, labels)
+function stats.error_matrix(guesses, labels, num_labels)
 
   local counts = true
 
-  local score0 = model:forward(data:narrow(1,1,1))
-  local data_size = data:size(1)
-  local num_labels = score0:size(1)
+  --local score0 = model:forward(data:narrow(1,1,1))
+  --local data_size = data:size(1)
+  --local num_labels = score0:size(1)
 
-  local indices = classify(model, data)
-
-  error_counts = {}
+  local error_counts = {}
 
   for i = -num_labels, num_labels do
     error_counts[i] = 0
@@ -132,7 +100,7 @@ function stats.error_matrix(model, data, labels)
 
   for i = 1, data_size do
     local gt = labels[i]
-    local guess = indices[i]
+    local guess = guesses[i]
     local err = gt - guess
     error_counts[err] = error_counts[err] + 1
   end
@@ -150,5 +118,59 @@ function stats.error_matrix(model, data, labels)
   
 end
 
+function stats.classify(model, loader, split)
+  local BATCH_SIZE = 100
+
+  local data_size = data_size(loader, split)
+
+  data0, _ = loader:getBatch{batch_size = 1, split = split}
+  local score0 = model:forward(data0:narrow(1,1,1))
+  local num_labels = score0:size(1)
+
+  local scores = torch.Tensor(data_size, num_labels)
+  local gt = torch.Tensor(data_size)
+
+  -- Process data in batches
+  local i = 1
+  while i < data_size do
+    local b = math.min(BATCH_SIZE, data_size - i + 1)
+    local batch_data, batch_data_y = loader:getBatch{batch_size = b, split = split}
+    local batch_scores = model:forward(batch_data)
+    
+    --print("b: " .. tostring(b))
+    --print(batch_scores:size())
+    --print(scores:size())
+    scores[{{i,i+b-1}}] = batch_scores
+    gt[{{i,i+b-1}}] = batch_data_y
+    i = i+b
+  end
+
+  -- Find number of generated labels that match ground truth labels
+  local maxs, guesses = torch.max(scores, 2)
+  guesses = torch.reshape(guesses, guesses:size(1))
+
+  --print("Guesses:")
+  --print(guesses:size())
+  --print("Ground Truth:")
+  --print(gt:size())
+
+  return guesses, gt, num_labels
+end
+
+function data_size(loader, split)
+  local size = -1
+  if split == 'train' then
+    size = loader:getTrainSize()
+  elseif split == 'val' then
+    size = loader:getValSize()
+  elseif split == 'test' then
+    size = loader:getTestSize()
+  end
+  assert(size > 0, "Data size cannot be <= 0")
+
+  return size
+end
+
 return stats
+
 
